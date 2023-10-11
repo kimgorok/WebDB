4988c99ad2b7d916091c93fdf83feec5a5f16f53
kimgorok <kjyse2@naver.com>
2023-10-12 08:56:08 +0900

    과제2

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// 201935250 김현중
+
 const express = require("express");
 const app = express();
 
@@ -6,39 +8,38 @@ app.set("view engine", "ejs");
 
 var db = require("./lib/db");
 var topic = require("./lib/topic");
-// const author = require("./lib/author");
-
-// 이벤트리스너 get.
+var author = require("./lib/author");
 
 app.get("/", (req, res) => {
   topic.home(req, res);
 });
-// app.get("/author", (req, res) => {
-//   author.author(req, res);
-// });
 
-app.get("/page/:pageId", (req, res) => {
-  topic.page(req, res);
+app.get("/author", (req, res) => {
+  author.home(req, res);
 });
 
-app.get("/create", (req, res) => {
-  topic.create(req, res);
+app.get("/author/page/:pageId", (req, res) => {
+  author.page(req, res);
 });
 
-app.post("/create_process", (req, res) => {
-  topic.create_process(req, res);
+app.get("/author/create", (req, res) => {
+  author.create(req, res);
 });
 
-app.get("/update/:pageId", (req, res) => {
-  topic.update(req, res);
+app.post("/author/create_process", (req, res) => {
+  author.create_process(req, res);
 });
 
-app.post("/update_process", (req, res) => {
-  topic.update_process(req, res);
+app.get("/author/update/:pageId", (req, res) => {
+  author.update(req, res);
 });
 
-app.get("/delete/:pageId", (req, res) => {
-  topic.delete_process(req, res);
+app.post("/author/update_process", (req, res) => {
+  author.update_process(req, res);
+});
+
+app.get("/author/delete/:pageId", (req, res) => {
+  author.delete_process(req, res);
 });
 
 app.get("/favicon.ico", (req, res) => res.writeHead(404));
